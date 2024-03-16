03c1b85e057a04d43a755e11b3af62b9ec54429b
Joshua Coles <158748501+JoshuaColes98@users.noreply.github.com>
2024-03-16 22:21:13 +0000

    Initial commit

---
+++ pong-game/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace pong_game
+{
+    partial class Pong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pong));
+            this.player = new System.Windows.Forms.PictureBox();
+            this.computer = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.playerScoreLabel = new System.Windows.Forms.Label();
+            this.computerScoreLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.GreenYellow;
+            this.player.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.player.Location = new System.Drawing.Point(58, 336);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(60, 240);
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // computer
+            // 
+            this.computer.BackColor = System.Drawing.Color.Red;
+            this.computer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.computer.Location = new System.Drawing.Point(1257, 336);
+            this.computer.Name = "computer";
+            this.computer.Size = new System.Drawing.Size(60, 240);
+            this.computer.TabIndex = 2;
+            this.computer.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ball.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ball.Image = ((System.Drawing.Image)(resources.GetObject("ball.Image")));
+            this.ball.Location = new System.Drawing.Point(786, 374);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(45, 45);
+            this.ball.TabIndex = 3;
+            this.ball.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // playerScoreLabel
+            // 
+            this.playerScoreLabel.AutoSize = true;
+            this.playerScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.playerScoreLabel.Font = new System.Drawing.Font("Play Pretend", 11.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.playerScoreLabel.Location = new System.Drawing.Point(12, 19);
+            this.playerScoreLabel.Name = "playerScoreLabel";
+            this.playerScoreLabel.Size = new System.Drawing.Size(224, 39);
+            this.playerScoreLabel.TabIndex = 4;
+            this.playerScoreLabel.Text = "Player: 0";
+            // 
+            // computerScoreLabel
+            // 
+            this.computerScoreLabel.AutoSize = true;
+            this.computerScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.computerScoreLabel.Font = new System.Drawing.Font("Play Pretend", 11.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.computerScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.computerScoreLabel.Location = new System.Drawing.Point(1050, 19);
+            this.computerScoreLabel.Name = "computerScoreLabel";
+            this.computerScoreLabel.Size = new System.Drawing.Size(278, 39);
+            this.computerScoreLabel.TabIndex = 5;
+            this.computerScoreLabel.Text = "Computer: 0";
+            // 
+            // Pong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1376, 880);
+            this.Controls.Add(this.computerScoreLabel);
+            this.Controls.Add(this.playerScoreLabel);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.computer);
+            this.Controls.Add(this.player);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Pong";
+            this.Text = "  Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox computer;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label playerScoreLabel;
+        private System.Windows.Forms.Label computerScoreLabel;
+    }
+}
+
---
+++ pong-game/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pong_game
+{
+    public partial class Pong : Form
+    {
+        int ballXspeed = 4;
+        int ballYspeed = 4;
+        int speed = 2;
+        Random random = new Random();
+        bool goDown, goUp;
+        int computer_speed_change = 50;
+        int playerScore = 0;
+        int computerScore = 0;
+        int playerSpeed = 8;
+        int[] i = { 5, 6, 8, 9 };
+        int[] j = { 10, 9, 8, 11, 12 };
+        bool isPaused = false; // New variable to track pause state
+
+        public Pong()
+        {
+            InitializeComponent();
+            MakeBallPictureBoxRounded();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            if (!isPaused) // Check if the game is not paused
+            {
+                ball.Top -= ballYspeed;
+                ball.Left -= ballXspeed;
+
+                playerScoreLabel.Text = "Player: " + playerScore;
+                computerScoreLabel.Text = "Computer: " + computerScore;
+
+                if (ball.Top < 0 || ball.Bottom > this.ClientSize.Height)
+                {
+                    ballYspeed = -ballYspeed;
+                }
+                if (ball.Left < -2)
+                {
+                    ball.Left = 300;
+                    ballXspeed = -ballXspeed;
+                    computerScore++;
+                }
+
+                if (ball.Right > this.ClientSize.Width + 2)
+                {
+                    ball.Left = 300;
+                    ballXspeed = -ballXspeed;
+                    playerScore++;
+                }
+
+                if (computer.Top <= 1)
+                {
+                    computer.Top = 0;
+                }
+                else if (computer.Bottom >= this.ClientSize.Height)
+                {
+                    computer.Top = this.ClientSize.Height - computer.Height;
+                }
+
+                if (ball.Top < computer.Top + (computer.Height / 2) && ball.Left > 300)
+                {
+                    computer.Top -= speed;
+                }
+
+                if (ball.Top > computer.Top + (computer.Height / 2) && ball.Left > 300)
+                {
+                    computer.Top += speed;
+                }
+
+                computer_speed_change -= 1;
+
+                if (computer_speed_change < 0)
+                {
+                    speed = i[random.Next(i.Length)];
+                    computer_speed_change = 50;
+                }
+
+                if (goDown && player.Top + player.Height < this.ClientSize.Height)
+                {
+                    player.Top += playerSpeed;
+                }
+
+                if (goUp && player.Top > 0)
+                {
+                    player.Top -= playerSpeed;
+                }
+
+                CheckCollision(ball, player, player.Right + 5);
+                CheckCollision(ball, computer, computer.Left - 35);
+
+                if (computerScore > 5)
+                {
+                    GameOver("Unlucky, try again?");
+                }
+                else if (playerScore > 5)
+                {
+                    GameOver("Congratulations, you won! Want to play again?");
+                }
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+            else if (e.KeyCode == Keys.Space) // Handle spacebar for pause
+            {
+                isPaused = !isPaused; // Toggle pause state
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+        }
+
+        private void CheckCollision(PictureBox PicOne, PictureBox PicTwo, int offset)
+        {
+            if (PicOne.Bounds.IntersectsWith(PicTwo.Bounds))
+            {
+                PicOne.Left = offset;
+
+                int x = j[random.Next(j.Length)];
+                int y = j[random.Next(j.Length)];
+
+                if (ballXspeed < 0)
+                {
+                    ballXspeed = x;
+                }
+                else
+                {
+                    ballXspeed = -x;
+                }
+
+                if (ballYspeed < 0)
+                {
+                    ballYspeed = -y;
+                }
+                else
+                {
+                    ballYspeed = y;
+                }
+            }
+        }
+
+        private void MakeBallPictureBoxRounded()
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(ball.ClientRectangle);
+            ball.Region = new Region(path);
+        }
+
+        private void GameOver(string message)
+        {
+            GameTimer.Stop();
+            DialogResult result = MessageBox.Show(message, "Game Over! ", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                RestartGame();
+            }
+            else if (result == DialogResult.No)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void RestartGame()
+        {
+            computerScore = 0;
+            playerScore = 0;
+            ballXspeed = ballYspeed = 4;
+            computer_speed_change = 50;
+            GameTimer.Start();
+        }
+    }
+}
